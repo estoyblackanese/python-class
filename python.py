e2449740eb1f6fc4455c4de3a3e18619c9e890c0
Asnel A. S. Valcin <A2nelito@A2MBP.local>
2018-11-11 19:08:26 -0500

    my python

---
+++ python.py
@@ -0,0 +1,10 @@
+#! python3
+from flask import (
+    Flask,
+    render_template
+)
+app = Flask(_name_, template_folder="templates")
+
+@app.route('/')
+def home:
+   return render_template('home.html')
